3c8a995ba02023a1568528742c23fe20404ef26b
albertarlington115 <notuser100@gmail.com>
2020-05-11 20:53:31 -0500

    Ecard create finished

--- src/views/Ecards/EcardCreate.js
+++ src/views/Ecards/EcardCreate.js
@@ -10,10 +10,12 @@ import {
     Image,
     Picker,
     TouchableHighlight,
-    Text
+    Text,
+    Alert
   } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import {Overlay} from 'react-native-elements';
+import ImagePicker from 'react-native-image-picker';
 // Connection related importss
 import { ConnectionWrapper } from '../../../connectionHelpers/ConnectionWrapper';
 import { hasInternetConnection } from '../../../connectionHelpers/hasInternetConnection';
@@ -22,14 +24,13 @@ import {OK, FAIL} from '../../../res/api/hostInfo';
 import COLORS from '../../../res/colors';
 import IMAGES from '../../../res/images';
 import commonStyles from '../../../res/commonStyles';
-import ImagePicker from 'react-native-image-picker';
 import moment from 'moment';
 import {api} from '../../../res/api/api';
 import { ScrollView } from 'react-native-gesture-handler';
   export class EcardCreate extends Component{
 
     constructor(props){
-
+        
         super(props);
         const {height, width} = Dimensions.get("window");
         this.height = height;
@@ -40,6 +41,10 @@ import { ScrollView } from 'react-native-gesture-handler';
 
             loading:false,
             hasInternet: true,
+            image_uri:IMAGES.placeholder,
+            image_value:IMAGES.placeholder,
+            image:"",
+            image_data:"",
             message_title:"",
             message_body:"",
             message_destinatary:{
@@ -51,6 +56,7 @@ import { ScrollView } from 'react-native-gesture-handler';
                 }
 
             },
+            message_destinatary_str:"No hay seleccionado",
             members:[] 
 
         }
@@ -96,6 +102,7 @@ import { ScrollView } from 'react-native-gesture-handler';
                 marginBottom:2,
                 marginLeft:2,
                 textAlign: "left",
+                textAlignVertical:"top",
                 justifyContent:"flex-start",
                 alignContent:"flex-start",
                 padding:10,
@@ -107,6 +114,28 @@ import { ScrollView } from 'react-native-gesture-handler';
                 
     }
 
+    //************************Ask for picture**************************/
+    askForPicture(context){
+        
+        ImagePicker.showImagePicker((e)=>{
+            
+            context.setState({
+
+                // image_uri: "data:" + e.type + ";" +"base64," + e.data,
+                image_uri: e.uri,
+                image_data:e.data,
+                image_type:e.type,
+                image_value:{
+
+                    uri:"data:image/jpg;base64," + e.data
+
+                },
+
+            });
+
+        });
+
+    }
 
     //************************Render picker options**************************/
 
@@ -117,9 +146,61 @@ import { ScrollView } from 'react-native-gesture-handler';
             console.log(value);
             return (
 
-                <Picker.Item label={value["user"]["full_name"]} value={value["id"]} key={index} />
+                <Picker.Item label={value["user"]["full_name"]} value={value["user"]["full_name"]} key={index} />
 
             )
+
+        });
+
+    }
+
+    //************************Create form data*/
+    createFormData(){
+
+        let fd = new FormData();
+
+        let img = {
+
+            uri: this.state.image_uri,
+            type:this.state.image_type,
+            name:"image.jpeg"         
+
+        };
+        console.log("form-data");
+        console.log(img);        
+
+        fd.append("image", img);
+
+        fd.append("title", this.state.message_title);
+        fd.append("body", this.state.message_body);
+        fd.append("receiver", this.state.message_destinatary["id"]);
+        console.log(this.state.message_destinatary["id"]);
+        return fd;
+
+    }
+
+    sendCard(){
+
+        let form_data = this.createFormData();
+
+        this.setState({
+
+            loading:true
+
+        })
+        api.post_ecard(form_data).then((response)=>{
+
+            if(response["status"]==OK){
+
+                Alert.alert("Éxito", "El mensaje se ha enviado con éxito");
+                Actions.pop();
+
+            }else{
+
+                Alert.alert("Error", "Ha habido un error");
+
+            }            
+            this.setState({loading:false});
 
         });
 
@@ -146,30 +227,30 @@ import { ScrollView } from 'react-native-gesture-handler';
     //************************Métodos de lifecycle que no son render */
     componentWillMount(){
         
-        // api.getGroupsMembers().then(response=>{
+        api.getGroupsMembers().then(response=>{
 
-        //     console.log("Response");
-        //     if(response["status"] == OK){
+            console.log("Response");
+            if(response["status"] == OK){
 
-        //         console.log("Succedded");
-        //         this.setState({members:[...response]})
-        //         console.log(this.state);
+                console.log("Succedded");
+                this.setState({members:[...response]})
+                console.log(this.state);
 
-        //     }else{
+            }else{
 
-        //         this.setState({members:[...response]})
-        //         console.log("FAILED");
+                this.setState({members:[...response]})
+                console.log("FAILED");
 
-        //     };
+            };
 
-        //     this.setState({loading:false});
+            this.setState({loading:false});
 
-        // });
+        });
 
     }
     
     render(){
-        // global.rol = 'COLLABORATOR'  
+        // global.rol = 'COLLABORATOR'  ssss
         let c_style = commonStyles(this);      
         return(
             <ConnectionWrapper
@@ -181,11 +262,11 @@ import { ScrollView } from 'react-native-gesture-handler';
                     
                     <ScrollView  style={{width:this.width, height:this.height}} contentContainerStyle={this.style.main}>
 
-                        <TouchableOpacity style={this.style.image}>
+                        <TouchableHighlight style={this.style.image} onPress={()=>{this.askForPicture(this)}}>
 
-                            
+                           <Image style={this.style.image} source={this.state.image_value}/> 
 
-                        </TouchableOpacity>
+                        </TouchableHighlight>
 
                         <TextInput 
                             style={this.style.textfield}
@@ -196,9 +277,18 @@ import { ScrollView } from 'react-native-gesture-handler';
                         />
 
                         <Picker
-                            selectedValue={this.state.message_destinatary["user"]["fullname"]}
+                            selectedValue={this.state.message_destinatary_str}
                             style={this.style.textfield}
-                            onValueChange={(message_destinatary)=>this.setState({message_destinatary})}
+                            onValueChange={(message_destinatary_str, index)=>{
+
+                                this.setState({message_destinatary_str});
+                                this.setState({
+
+                                    message_destinatary:{...this.state.members[index]}
+
+                                });
+
+                            }}
                         >
 
                             {this.renderPickerOptions(this.state.members)}
@@ -208,23 +298,26 @@ import { ScrollView } from 'react-native-gesture-handler';
                         <TextInput 
                             style={this.style.textarea}
                             multiline={true}
+                            numberOfLines={-1}
                             placeholder="Mensaje..."
                             placeholderTextColor="gray"
-                            value={this.state.message_title}
+                            value={this.state.message_body}
                             onChangeText={(message_body)=>this.setState({message_body})}
                         />
 
                         <TouchableHighlight style={{width:this.width*0.5,                                 
-                                padding:13, 
+                                padding:20,                                 
                                 marginTop:10,
                                 textAlign:"center",
                                 marginBottom:15,
+                                borderRadius:4,
+                                elevation:5,
                                 backgroundColor:COLORS.buttons,
                                 ...c_style.rounded_button
                             }}
-                                onPress={() => this.verificar()}
-                                underlayColor="#cc3600">
-                                <Text style={{...c_style.text_button}}>Enviar e-card</Text> 
+                                onPress={() => this.sendCard()}
+                                underlayColor="white">
+                                <Text style={{...c_style.text_button, textAlign:"center", color:"white", fontSize:15}}>Enviar e-card</Text> 
                         </TouchableHighlight>
 
                     </ScrollView>                   
